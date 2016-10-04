fd382cfc048367d277593d3237fefcc888e8596b
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-10-04 14:49:03 +0200

    Added jest + reformatted code

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
 module.exports = {
-    parser: 'babel-eslint',
-    env: {
-        browser: true, // We write for browser
-        es6: true, // using ES6 syntax
-        commonjs: true, // sometimes requiring modules in CommonJS
-        mocha: true // We use Mocha for tests
-    },
-    extends: [
-        'eslint-config-airbnb/base',
-        'eslint-config-airbnb/rules/strict',
-        'eslint-config-airbnb/rules/react'
-    ].map(require.resolve),
-    plugins: [
-        'flow-vars'
-    ],
-    rules: {
-        'semi': [2, 'never'],
-        'indent': [2, 4, {
-            'SwitchCase': 1
-        }],
-        'comma-dangle': [2, 'never'],
-        'jsx-quotes': [2, 'prefer-double'],
-        'padded-blocks': [0, 'never'],
-        'id-length': [2, {'exceptions': ['b']}],
-        'new-cap': [2, {
-            'newIsCap': true,
-            'capIsNewExceptions': ['List', 'Map', 'Set']
-        }],
-        'no-console': 2,
-        'no-debugger': 2,
-        'guard-for-in': 0,
-        'object-curly-spacing': [2, 'always'],
-        'template-curly-spacing': [2, 'always'],
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    browser: true,
+    commonjs: true,
+    mocha: true,
+    jest: true
+  },
+  extends: [
+    'eslint-config-airbnb/base',
+    'eslint-config-airbnb/rules/strict',
+    'eslint-config-airbnb/rules/react'
+  ].map(require.resolve),
+  plugins: [
+    'flow-vars'
+  ],
+  rules: {
+    'semi': [2, 'never'],
+    'indent': [2, 4, {
+      'SwitchCase': 1
+    }],
+    'comma-dangle': [2, 'never'],
+    'jsx-quotes': [2, 'prefer-double'],
+    'padded-blocks': [0, 'never'],
+    'id-length': [2, { 'exceptions': ['b'] }],
+    'new-cap': [2, {
+      'newIsCap': true,
+      'capIsNewExceptions': ['List', 'Map', 'Set']
+    }],
+    'no-console': 2,
+    'no-debugger': 2,
+    'guard-for-in': 0,
+    'object-curly-spacing': [2, 'always'],
+    'template-curly-spacing': [2, 'always'],
 
-        // React
-        'react/jsx-indent-props': [2, 4],
-        'react/sort-comp': [2, {
-            order: [
-                'flow-types',
-                'static-methods',
-                'lifecycle',
-                'everything-else',
-                'render'
-            ],
-            groups: {
-                'flow-types': [
-                    'props',
-                    'state'
-                ]
-            }
-        }],
+    // React
+    'react/jsx-indent-props': [2, 4],
+    'react/sort-comp': [2, {
+      order: [
+        'flow-types',
+        'static-methods',
+        'lifecycle',
+        'everything-else',
+        'render'
+      ],
+      groups: {
+        'flow-types': [
+          'props',
+          'state'
+        ]
+      }
+    }],
 
-        // flow-vars plugin
-        'flow-vars/define-flow-type': 1,
-        'flow-vars/use-flow-type': 1
-    }
+    // flow-vars plugin
+    'flow-vars/define-flow-type': 1,
+    'flow-vars/use-flow-type': 1
+  }
 };
